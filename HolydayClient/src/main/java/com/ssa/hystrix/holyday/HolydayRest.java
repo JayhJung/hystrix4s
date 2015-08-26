5d8f1088e9d6e0d5a399f592a5b427a1678e12a2
webstre <webstre@gmail.cm>
2015-08-26 21:49:37 +0900

    changed response var static to non-static

--- HolydayClient/src/main/java/com/ssa/hystrix/holyday/HolydayRest.java
+++ HolydayClient/src/main/java/com/ssa/hystrix/holyday/HolydayRest.java
@@ -5,7 +5,6 @@ import com.mashape.unirest.request.GetRequest;
 import com.ssa.hystrix.hello.CommandHolyday;
 
 public class HolydayRest{
-	static GetRequest response = null;
 
 	public static GetRequest get(final String url) {
 		
@@ -13,7 +12,7 @@ public class HolydayRest{
 		
 			@Override
 			public void holydayRun() {
-				response = new GetRequest(HttpMethod.GET, url);
+				response =  new GetRequest(HttpMethod.GET, url);
 			}
 			
 			@Override
@@ -22,7 +21,8 @@ public class HolydayRest{
 			}
 		};
 		command.execute();
-		return response;
+
+		return command.response;
 	}
 	
 	//TODO Extension Point : fallback을 계속해서 지정할 수 있도록 변경.
@@ -41,7 +41,7 @@ public class HolydayRest{
 			}
 		};
 		command.execute();
-		return response;
+		return command.response;
 	}
 	
 }
