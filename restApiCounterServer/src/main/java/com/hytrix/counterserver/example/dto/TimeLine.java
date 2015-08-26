1110fc1ab737b668c00cb23beb2edad3e3338d53
유병일 <>
2015-08-26 22:06:50 +0900

    카운트 서버

---
+++ restApiCounterServer/src/main/java/com/hytrix/counterserver/example/dto/TimeLine.java
@@ -0,0 +1,25 @@
+package com.hytrix.counterserver.example.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeLine {
+	private long userId;
+	private List articleList;
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public List getArticleList() {
+		return articleList;
+	}
+	public void setArticleList(List articleList) {
+		this.articleList = articleList;
+	}
+	
+	
+	
+	
+}
